08385c429616a04955038a1d22610cd1c2478ec5
MayronSouza <mayron.souza@gmz.solutions>
2021-01-01 15:02:27 -0300

    Configurando o Redux Saga, que disponibilizara um middleware para interceptar nossas actions antes que os reducers as escutem

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,25 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+
 import { ICartState } from './modules/Cart/types';
 
 import rootReducers from './modules/rootReducers';
+import rootSaga from './modules/rootSaga';
 
 export interface IState {
   cart: ICartState;
 }
 
-const store = createStore(rootReducers);
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(...middlewares)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
